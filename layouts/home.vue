b36a37cdf898631a0de1a3f7d1ccd5f19037dc4e
moshemo <moshem@live.com>
2017-09-12 01:20:41 +0300

    Initial commit

---
+++ layouts/home.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app id="example-2" toolbar>
+
+    <site-main/>
+
+  </v-app>
+</template>
+
+<script>
+// import Drawer from './site/Drawer'
+// import Toolbar from './site/Toolbar'
+// import Main from './site/Main'
+
+// export default {
+//   components: {
+//     'siteDrawer': Drawer,
+//     'siteToolbar': Toolbar,
+//     'siteMain': Main
+//   },
+//   data () {
+//     return {
+//       items: [
+//         { title: 'Home', icon: 'dashboard' },
+//         { title: 'About', icon: 'question_answer' }
+//       ],
+//       right: null
+//     }
+//   }
+// }
+</script>
